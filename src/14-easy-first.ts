66804cc99362e825460c8a8ac1984e7f9dfa36ff
takasing <takaty2011@gmail.com>
2020-10-19 00:09:53 +0900

    other pattern

--- src/14-easy-first.ts
+++ src/14-easy-first.ts
@@ -1,3 +1,11 @@
 export type First<T extends any[]> =
   // lengthプロパティが0！
   T extends { length: 0 } ? never : T[0]
+  // inferで正規表現のキャプチャ的なことができるらしい
+  // => Pに相当する型を後で使える
+  // T extends [infer P, ...any[]] ? P : never
+  // T[0] extends T[number] ? T[0] : never
+  // keyof Tに0があるか?
+  // T[Extract<keyof T, '0'>]
+  // T extends [] ? never : T[0]
+  // T[number] extends never ? never : T[0]
